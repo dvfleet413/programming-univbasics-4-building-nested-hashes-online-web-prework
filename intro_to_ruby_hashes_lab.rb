e5e21d530de44857243cd5576dd06327494cb2f6
David Van Fleet <dvfleet413@gmail.com>
2019-12-17 22:24:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	    },
 	    :names => {
 	      :reading_railroad => {},
-	      :second => "",
+	      :pennsyvania_railroad => "",
 	      :third => "",
 	      :fourth => ""
 	    }
